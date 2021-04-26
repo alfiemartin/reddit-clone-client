c6cbf7384f28d40a7dfa1d748cdd51943c3cbdaa
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-04-26 16:41:22 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Reddit clone client</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
